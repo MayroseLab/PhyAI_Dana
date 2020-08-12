7575caf0061d0ece4d0bdf5ca288bd8535be6a78
DanaAzouri <danaazouri@gmail.com>
2020-08-12 11:51:15 +0300

    enable fixed model parameters

--- execute_programs/Phyml.py
+++ execute_programs/Phyml.py
@@ -36,8 +36,21 @@ PHYML_SCRIPT = "/groups/itay_mayrose/shiranabad/applications/jmodeltest-2.1.7/ex
 
 def create_phyml_exec_line(msa_file_full_path, base_model, pinv, gamma, topology_tag, tree_file=False):
 	run_id = topology_tag
-	execution_tags = " ".join(PHYML_MODEL_TAGS[base_model] +
-							  [PHYML_PINV_TAGS[pinv], PHYML_GAMMA_TAGS[gamma],
+
+	# enable fixed model parameters
+	if topology_tag == "br":
+		stats_filpath_for_params = msa_file_full_path + "_phyml_stats_bionj.txt"
+		params_dict = parse_phyml_stats_output(stats_filpath_for_params)
+		pinv_tags = "-v {}".format(params_dict["pInv"])
+		gamma_tags = "-a {} -c 4".format(params_dict["gamma"])
+		freq_tags = "-f {},{},{},{}".format(params_dict["fA"],params_dict["fC"],params_dict["fG"],params_dict["fT"])   # "fA", "fC", "fG", "fT"
+		model_tags = [PHYML_SUBS_RATES_TAGS[3], freq_tags]
+	else:
+		pinv_tags = PHYML_PINV_TAGS[pinv]
+		gamma_tags = PHYML_GAMMA_TAGS[gamma]
+		model_tags = PHYML_MODEL_TAGS[base_model]
+
+	execution_tags = " ".join(model_tags + [pinv_tags, gamma_tags,
 							   PHYML_TOPOLOGY_TAGS[topology_tag], PHYML_GENERAL_TAGS, "--run_id " + run_id])
 	if tree_file:
 		execution_tags += " -u " + tree_file
