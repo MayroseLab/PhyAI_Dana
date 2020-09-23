1dbc64553c8a03d80fe3e2600dea91a10641921e
DanaAzouri <danaazouri@gmail.com>
2020-09-23 18:54:04 +0300

    reorganize archi - avoid i/o by not copying orig_msa and rearrs files and the WHOLE dir hierarchy!! PREPARING FOR RUNNING ALL

--- execute_programs/SPR_move.py
+++ execute_programs/SPR_move.py
@@ -1,15 +1,18 @@
 import sys
+
 sys.path.append("/groups/itay_mayrose/danaazouri/PhyAI/code/")
 
 from defs import *
-from ete3 import Tree, PhyloTree
-from execute_programs.Phyml import run_phyml
-from parsing.parse_phyml import parse_phyml_stats_output
 from utils.msa_functions import *
 from data_processing.traverse_data_dirs import traverse_data_dirs
 from summary.collect_SPR_features import *
+from subprocess import Popen, PIPE, STDOUT
+from parsing.parse_raxml_NG import parse_raxmlNG_content
+from execute_programs.RAxML_NG import extract_model_params
+import csv
 
 ML_SOFTWARE = 'RAxML_NG'     # could be phyml | RAxML_NG
+RAXML_NG_SCRIPT = "raxml-ng"
 
 
 
@@ -63,13 +66,12 @@ def add_internal_names(tree_file, tree_file_cp_no_internal, t_orig):
 
 
 def get_tree(ds_path, msa_file, rewrite_phylip):
-	#suf = "bionj" if not RANDOM_TREE_DIRNAME in ds_path else "br"
 	suf = "bionj" if not RANDOM_TREE_DIRNAME in ds_path else "br"
 	tree_file = ds_path + PHYML_TREE_FILENAME.format(suf) if ML_SOFTWARE == 'phyml' else ds_path + RAXML_TREE_FILENAME    # if software=='RAxML_NG'
 	if rewrite_phylip:
 		rewrite_in_phylip(msa_file)     # for one-time use on new ds
 	tree_file_cp_no_internal = ds_path + PHYML_TREE_FILENAME.format(suf + "_no_internal") if ML_SOFTWARE == 'phyml' else ds_path + RAXML_TREE_FILENAME + "_no_internal"
-	#print("\n################ tree file:" + tree_file + " ################\n")
+
 	if not os.path.exists(tree_file_cp_no_internal):
 		t_orig = PhyloTree(newick=tree_file, alignment=msa_file, alg_format="iphylip", format=1)
 		add_internal_names(tree_file, tree_file_cp_no_internal, t_orig)
@@ -77,16 +79,6 @@ def get_tree(ds_path, msa_file, rewrite_phylip):
 		t_orig = PhyloTree(newick=tree_file, alignment=msa_file, alg_format="iphylip", format=3)
 
 	return t_orig
-
-
-def save_rearr_file(trees_dirpath, rearrtree, filename, runover=False):
-	if not os.path.exists(trees_dirpath):
-		os.makedirs(trees_dirpath)
-	tree_path = trees_dirpath + filename + ".txt"
-	if runover or not os.path.exists(tree_path):
-		rearrtree.write(format=1, outfile=tree_path)
-
-	return tree_path
 
 
 def call_ml_software(tree_dirpath, file_name, msa_file, runover, job_priority, cpmsa=False, software=ML_SOFTWARE):
@@ -104,13 +96,33 @@ def call_ml_software(tree_dirpath, file_name, msa_file, runover, job_priority, c
 	os.system(cmd)
 
 
-def remove_redundant_nodes(tree, ntaxa, node_name = None):
-	if len(tree.get_descendants()) > ntaxa * 2 - 3:
-		tree.get_tree_root().children[0].delete(preserve_branch_length=True)
-	#if node_name and len(tree.get_descendants()) > ntaxa * 2 - 3:
-	#	tree.search_nodes(name=node_name)[0].up.delete(preserve_branch_length=True)
+def call_raxml_mem(tree_str, msa_file, rates, pinv, alpha, freq):
+	model_line_params = 'GTR{rates}+I{pinv}+G{alpha}+F{freq}'.format(rates="{{{0}}}".format("/".join(rates)),
+									 pinv="{{{0}}}".format(pinv), alpha="{{{0}}}".format(alpha),
+									 freq="{{{0}}}".format("/".join(freq)))
 
-	return tree
+	# first, create tree file in memory and save it:
+	tree_rampath = "/dev/shm/" + msa_file.split(SEP)[-1] + "tree"  # the var is the str: tmp{dir_suffix}
+	try:
+		with open(tree_rampath, "w") as fpw:
+			fpw.write(tree_str)
+
+		p = Popen([RAXML_NG_SCRIPT, '--evaluate', '--msa', msa_file,'--threads', '1', '--opt-branches', 'on', '--opt-model', 'off', '--model', model_line_params, '--nofiles', '--tree', tree_rampath],
+				  stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+		raxml_stdout = p.communicate()[0]
+		raxml_output = raxml_stdout.decode()
+
+		res_dict = parse_raxmlNG_content(raxml_output)
+		ll = res_dict['ll']
+
+	except Exception as e:
+		print(e)
+		exit()
+	finally:
+		os.remove(tree_rampath)
+
+	return ll
+
 
 
 def create_SPR_job(dataset_path, step_number, tree_type, rewrite_phy, runover):
@@ -127,34 +139,62 @@ def create_SPR_job(dataset_path, step_number, tree_type, rewrite_phy, runover):
 
 
 
-
-def all_SPR(ds_path, tree=None, rewrite_phylip=False, runover=False, job_priority=-1):
+def all_SPR(ds_path, outpath, tree=None, rewrite_phylip=False):
 	orig_msa_file = ds_path + MSA_PHYLIP_FILENAME
-	ntaxa = get_msa_properties(orig_msa_file)[0]
-	seqs_dict = get_seqs_dict(orig_msa_file)
 	t_orig = get_tree(ds_path, orig_msa_file, rewrite_phylip) if not tree else PhyloTree(newick=tree, alignment=orig_msa_file, alg_format="iphylip", format=1)
 	t_orig.get_tree_root().name = ROOTLIKE_NAME if not tree else ROOTLIKE_NAME+"_2"
-	for i, prune_node in enumerate(t_orig.iter_descendants("levelorder")):
-		prune_name = prune_node.name
-		nname, subtree1, subtree2 = prune_branch(t_orig, prune_name) # subtree1 is the pruned subtree. subtree2 is the remaining subtree
-		subtrees_dirpath = SEP.join([ds_path, REARRANGEMENTS_NAME+"s", prune_name, "{}", ""])
-		#call_phyml_ll(subtrees_dirpath, subtree1, subtree2, seqs_dict, runover=runover) # cal phyml foreach subtree + truncated msa
-		save_rearr_file(subtrees_dirpath.format(SUBTREE1), subtree1, filename=SUBTREE1,runover=runover)  # will print it out WITHOUT the root NAME !
-		save_rearr_file(subtrees_dirpath.format(SUBTREE2), subtree2, filename=SUBTREE2,runover=runover)  # will print it out WITHOUT the root NAME !
+	OUTPUT_TREES_FILE = TREES_PER_DS.format(ds_path, '1')
+	with open(OUTPUT_TREES_FILE, "w", newline='') as fpw:
+		csvwriter = csv.writer(fpw)
+		csvwriter.writerow(['', 'prune_name', 'rgft_name', 'newick'])
 
-		for j, rgft_node in enumerate(subtree2.iter_descendants("levelorder")):
-			rgft_name = rgft_node.name
-			if nname == rgft_name: # if the rgrft node is the one that was pruned
-				continue
+	# first, copy msa file to memory and save it:
+	msa_rampath = "/dev/shm/tmp" + ds_path.split(SEP)[-2] #  to be on the safe side (even though other processes shouldn't be able to access it)
 
-			full_tree_dirpath = subtrees_dirpath.format(rgft_name)
-			if runover or not os.path.exists(full_tree_dirpath + REARRANGEMENTS_NAME + ".txt"):
-				full_tree = regraft_branch(subtree2, rgft_node, subtree1, rgft_name, nname)
-				#remove_redundant_nodes(full_tree, ntaxa, rgft_name)
-				save_rearr_file(full_tree_dirpath, full_tree, filename=REARRANGEMENTS_NAME, runover=runover)
-			call_ml_software(full_tree_dirpath, REARRANGEMENTS_NAME, orig_msa_file, runover, job_priority, cpmsa=True)
-		#exit()
+	with open(orig_msa_file) as fpr:
+		msa_str = fpr.read()
 
+	try:
+		with open(msa_rampath, "w") as fpw:
+			fpw.write(msa_str)  # don't write the msa string to a variable (or write and release it)
+		msa_str = ''
+
+		rates, pinv, alpha, freq = extract_model_params(msa_rampath, ds_path, 'raxml')
+		df = pd.DataFrame()
+		for i, prune_node in enumerate(t_orig.iter_descendants("levelorder")):
+			prune_name = prune_node.name
+			nname, subtree1, subtree2 = prune_branch(t_orig, prune_name) # subtree1 is the pruned subtree. subtree2 is the remaining subtree
+			with open(OUTPUT_TREES_FILE, "a", newline='') as fpa:
+				csvwriter = csv.writer(fpa)
+				csvwriter.writerow([str(i)+",0", prune_name, SUBTREE1, subtree1.write(format=1)])
+				csvwriter.writerow([str(i)+",1", prune_name, SUBTREE2, subtree2.write(format=1)])
+
+			for j, rgft_node in enumerate(subtree2.iter_descendants("levelorder")):
+				ind = str(i) + "," + str(j)
+				rgft_name = rgft_node.name
+				if nname == rgft_name: # if the rgrft node is the one that was pruned
+					continue
+
+				rearr_tree_str = regraft_branch(subtree2, rgft_node, subtree1, rgft_name, nname).write(format=1)
+				### save tree to file by using "append"
+				with open(OUTPUT_TREES_FILE, "a", newline='') as fpa:
+					csvwriter = csv.writer(fpa)
+					csvwriter.writerow([ind, prune_name, rgft_name, rearr_tree_str])
+
+				ll_rearr = call_raxml_mem(rearr_tree_str, msa_rampath, rates, pinv, alpha, freq)
+				df.loc[ind, "prune_name"], df.loc[ind, "rgft_name"] = prune_name, rgft_name
+				df.loc[ind, "ll"] = ll_rearr
+
+		df["orig_ds_ll"] = float(parse_raxmlNG_output(ds_path + RAXML_STATS_FILENAME)['ll'])
+		df.to_csv(outpath.format("prune"))
+		df.to_csv(outpath.format("rgft"))
+
+	except Exception as e:
+		print('xxxxx')
+		print(e)
+		exit()
+	finally:
+		os.remove(msa_rampath)
 	return
 
 
@@ -165,7 +205,6 @@ if __name__ == '__main__':
 	parser.add_argument('--dataset_path', '-ds', default=None)
 	parser.add_argument('--runover', '-r', default=False, action='store_true')
 	parser.add_argument('--rewrite_in_phylip', '-phy', default=False, action='store_true')
-	parser.add_argument('--cp_internal', '-cp', default=False, action='store_true')
 	parser.add_argument('--tree_type', '-ttype', default='bionj')  # could be bionj or random
 	parser.add_argument('--index_to_start_run', '-istart', default=False)
 	parser.add_argument('--nline_to_run', '-nlines', default=False)		 # number of lines from the dataset (int): n^2
@@ -175,24 +214,19 @@ if __name__ == '__main__':
 	dataset_path = args.dataset_path
 	if dataset_path:
 		dataset_path = dataset_path if args.tree_type == 'bionj' else dataset_path + RANDOM_TREE_DIRNAME # if == 'random
-		outpath_prune = SUMMARY_PER_DS.format(dataset_path, "prune", "br", args.step_number)
-		outpath_rgft = SUMMARY_PER_DS.format(dataset_path, "rgft", "br", args.step_number)
-		# todo: uncomment the folowwing condition after running over the failed runings
-		#if not os.path.exists(outpath_rgft) or not os.path.exists(outpath_prune):
-		if args.step_number == "1":
-			res = all_SPR(dataset_path, tree=None, rewrite_phylip=args.rewrite_in_phylip, runover=args.runover, job_priority=-1)
-		else:   # run next step on previous' best tree
-			prev_step = str(int(args.step_number)-1)
-			dfr = pd.read_csv(TREES_PER_DS.format(dataset_path, prev_step), index_col=0)
-			df_sum = pd.read_csv(SUMMARY_PER_DS.format(dataset_path, "prune", "br", prev_step)).set_index('Unnamed: 0')
-			best_tree_id = df_sum["ll"].astype(float).idxmax()
-			tree_str = dfr.loc[best_tree_id, "newick"]
+		outpath = SUMMARY_PER_DS.format(dataset_path, "{}", "br", args.step_number)
+		if args.runover:
+			if args.step_number == "1":
+				res = all_SPR(dataset_path, outpath, tree=None, rewrite_phylip=args.rewrite_in_phylip)
+			else:   # run next step on previous' best tree
+				prev_step = str(int(args.step_number)-1)
+				dfr = pd.read_csv(TREES_PER_DS.format(dataset_path, prev_step), index_col=0)
+				df_sum = pd.read_csv(SUMMARY_PER_DS.format(dataset_path, "prune", "br", prev_step)).set_index('Unnamed: 0')
+				best_tree_id = df_sum["ll"].astype(float).idxmax()
+				tree_str = dfr.loc[best_tree_id, "newick"]
+				res = all_SPR(dataset_path, outpath, tree=tree_str, rewrite_phylip=args.rewrite_in_phylip)
 
-			res = all_SPR(dataset_path, tree=tree_str, rewrite_phylip=args.rewrite_in_phylip, runover=args.runover, job_priority=-1)
-
-		# after preforming all steps, parse results and delete all files
-		res = parse_neighbors_dirs(dataset_path, outpath_prune, outpath_rgft, args.step_number, args.cp_internal, tree_type=args.tree_type)
-		collect_features(dataset_path, args.step_number, outpath_prune, outpath_rgft, args.tree_type)
+		collect_features(dataset_path, args.step_number, outpath.format("prune"), outpath.format("rgft"), args.tree_type)
 	else:
 		csv_path = SUMMARY_FILES_DIR + CHOSEN_DATASETS_FILENAME
 		res = traverse_data_dirs(create_SPR_job, csv_path, (args.index_to_start_run, args.nline_to_run), args.step_number, args.tree_type, args.rewrite_in_phylip, args.runover)
