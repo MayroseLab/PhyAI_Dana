31902f0ae5167ec95f05b1c46c9d35054fa92134
DanaAzouri <danaazouri@gmail.com>
2020-10-12 17:07:33 +0300

    -

--- data_processing/temp_for_ML_workshop.py
+++ data_processing/temp_for_ML_workshop.py
@@ -4,7 +4,8 @@ import warnings
 warnings.filterwarnings("ignore")			# TEMP
 
 from defs import *
-from data_processing.create_starting_trees import phyml_for_ll
+from utils.msa_functions import *
+from data_processing.create_starting_trees import *
 SUBMISSION_DIR = DIRPATH + "submission_data/"
 
 
@@ -17,21 +18,28 @@ def copy_datasets(df, set):
 		dest_dirname = dataset_path.split("/")[-2]
 		
 		orig_msa = dataset_path + SEP + MSA_PHYLIP_FILENAME
-		dest_path_dir = SEP.join([SUBMISSION_DIR + set, dest_dirname, ""])
+		#dest_path_dir = SEP.join([SUBMISSION_DIR + set, dest_dirname, ""])
+		dest_path_dir = SEP.join(["/groups/itay_mayrose/danaazouri/PhyAI/ML_workshop/data/" + set, dest_dirname, ""])
 		if not os.path.exists(dest_path_dir):
 			os.makedirs(dest_path_dir)
-		
-		shutil.copy(orig_msa, dest_path_dir + MSA_PHYLIP_FILENAME_NOT_MASKED)
-		
+
+		shutil.copy(orig_msa, dest_path_dir + MSA_PHYLIP_FILENAME)
+		#if
+
+		exit()
+
+
 
 
 if __name__ == '__main__':
-	df_validation = pd.read_csv("/groups/itay_mayrose/danaazouri/PhyAI/validation_set2/summary_files/" + CHOSEN_DATASETS_FILENAME)
-	df_6000 = pd.read_csv(SUMMARY_FILES_DIR + CHOSEN_DATASETS_FILENAME)
-	df_3700 = pd.read_csv(SUMMARY_FILES_DIR + "scores_per_ds_28_1_ytransformed_exp_minus.csv")
-	df_additional = df_6000[~df_6000["path"].isin(df_3700["path"].unique())]
-	
-	copy_datasets(df_3700, "training_data")
+	#df_validation = pd.read_csv("/groups/itay_mayrose/danaazouri/PhyAI/validation_set2/summary_files/" + CHOSEN_DATASETS_FILENAME)
+	#df_6000 = pd.read_csv(SUMMARY_FILES_DIR + CHOSEN_DATASETS_FILENAME)
+	#df_3700 = pd.read_csv(SUMMARY_FILES_DIR + "scores_per_ds_28_1_ytransformed_exp_minus.csv")
+	#df_additional = df_6000[~df_6000["path"].isin(df_3700["path"].unique())]
+
+	df = pd.read_csv(SUMMARY_FILES_DIR + CHOSEN_DATASETS_FILENAME)
+	copy_datasets(df, "training_datasets")
+
 	#copy_datasets(df_additional, "additional_training_data")
 	#copy_datasets(df_validation, "validation_data")
 	
--- utils/trying.py
+++ utils/trying.py
@@ -3,15 +3,62 @@ sys.path.append("/groups/itay_mayrose/danaazouri/PhyAI/code/")
 import warnings
 
 from defs import *
-from Bio import Phylo
-import networkx, pylab
-TREE_PATH = "/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data_test/tree.txt"
+from Bio import SeqIO
+#import networkx, pylab
 
-tree = Phylo.read(TREE_PATH, "newick")
-net = Phylo.to_networkx(tree)
-pos = networkx.spring_layout(net)
-print(pos)
+#PATH = "/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/exampleSphaero/real_msa"
+#SeqIO.convert(PATH + '.nex', "nexus", PATH + '.phy', PHYLIP_FORMAT)
+
+def draw_phytree():
+    tree = "(((Sp004:0.00978979,Sp003:0.0388109)N2:0.0316201,Sp002:0.045075)N1:0.0837587,Sp001:0.00308638)ROOT;"
+    t = Tree(tree, format=1)
+    print(t.get_ascii(show_internal=True), "\n###########\n")
+    '''
+    ts = TreeStyle()
+    ts.show_leaf_name = True
+    # ts.rotation = 90
+    ts.mode = "c"
+    ts.arc_start = -180  # 0 degrees = 3 o'clock
+    ts.arc_span = 180
+    ts.scale = 200
+    ts.branch_vertical_margin = 15
+    ts.show_branch_length = True
+
+    nst_general = NodeStyle()
+    nst_general["fgcolor"] = "white"
+    for tn in [t]:
+        for l in tn.iter_leaves():
+            l.img_style = nst_general
+
+    nst1 = NodeStyle()
+    # nst1["bgcolor"] = "LightSteelBlue"n #"Moccasin"
+    nst1["fgcolor"] = "red"
+    nst1["size"] = 5
+
+    nst2 = NodeStyle()
+    nst2["fgcolor"] = "blue"
+    nst2["size"] = 5
+    '''
+    t.render(r"C:\Users\ItayMNB3\Dropbox\PhyloAI\\" + 'trees.png') #, tree_style=ts)
 
 
-networkx.draw(net)
-pylab.show()
+
+
+if __name__ == '__main__':
+    #draw_phytree()
+
+    df = pd.read_csv(SUMMARY_FILES_DIR + "with_preds_merged_20_1_5850_ytransformed_exp.csv", nrows=1000)
+
+    #corr_path = "/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/24518/"
+    corr_path = "/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/38663/"
+    df = df[df["path"] == corr_path]
+    #print(df[["d_ll_merged", "pred"]].corr(method='spearman'))
+    #print(df)
+
+    #sns.heatmap(corr, annot=True)
+    plt.scatter(df["d_ll_merged"].rank(), df["pred"].rank())
+    plt.xlabel("Rank/naccording to the log-likelihood", labelpad=13)
+    plt.ylabel("Rank/naccording to the model", labelpad=13)
+    plt.tight_layout()
+    plt.savefig(SUMMARY_FILES_DIR + "corr.png")
+    #plt.show()
