2db8f5aec1d88c98d3daa62e0de03ff2671f3e00
DanaAzouri <danaazouri@gmail.com>
2020-10-15 15:27:29 +0300

    prepare new data for ML_workshop

--- data_processing/temp_for_ML_workshop.py
+++ data_processing/temp_for_ML_workshop.py
@@ -6,27 +6,47 @@ warnings.filterwarnings("ignore")			# TEMP
 from defs import *
 from utils.msa_functions import *
 from data_processing.create_starting_trees import *
+from Bio import AlignIO
+from Bio.Align import MultipleSeqAlignment
+
 SUBMISSION_DIR = DIRPATH + "submission_data/"
 
 
 
+def trucate_to_20(msa):
+	alignment = AlignIO.read(msa, PHYLIP_FORMAT)
+	new_align = alignment[0:20, :]
+	trunc_msa = MultipleSeqAlignment(new_align)
+	AlignIO.write(trunc_msa, msa, PHYLIP_FORMAT)
+	rewrite_in_phylip(msa)
+
 
 def copy_datasets(df, set):
+	new_df = pd.DataFrame()
 	for index, row in df.iterrows():
 		dataset_path = row["path"]
-		dataset_name = re.search("(\/.*data2?\/.*\/)(.*\/.*)", row["path"]).group(2)
-		dest_dirname = dataset_path.split("/")[-2]
-		
 		orig_msa = dataset_path + SEP + MSA_PHYLIP_FILENAME
-		#dest_path_dir = SEP.join([SUBMISSION_DIR + set, dest_dirname, ""])
-		dest_path_dir = SEP.join(["/groups/itay_mayrose/danaazouri/PhyAI/ML_workshop/data/" + set, dest_dirname, ""])
+		ntaxa, nchars = get_msa_properties(orig_msa)
+		if ntaxa < 20:
+			continue
+
+		datapath = "/groups/itay_mayrose/danaazouri/PhyAI/ML_workshop/data/" + set
+		dest_dirname = dataset_path.split("/")[-2]
+		dest_path_dir = SEP.join([datapath, dest_dirname, ""])
+		new_df.loc[index, "path"] = dest_path_dir
 		if not os.path.exists(dest_path_dir):
 			os.makedirs(dest_path_dir)
+		msa = dest_path_dir + MSA_PHYLIP_FILENAME
+		shutil.copy(orig_msa, msa)
 
-		shutil.copy(orig_msa, dest_path_dir + MSA_PHYLIP_FILENAME)
-		#if
+		if ntaxa > 20:
+			trucate_to_20(msa)
 
-		exit()
+		generate_starting_trees(dest_path_dir, 'bionj')
+
+	new_df.to_csv("/groups/itay_mayrose/danaazouri/PhyAI/ML_workshop/datasets_20taxa.csv")
+
+
 
 
 
