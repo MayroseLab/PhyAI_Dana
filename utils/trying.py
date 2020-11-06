9238f20eef8c1313835d18d6380c8b69bc09e38c
DanaAzouri <danaazouri@gmail.com>
2020-11-07 01:10:45 +0200

    -

--- utils/trying.py
+++ utils/trying.py
@@ -85,21 +85,11 @@ if __name__ == '__main__':
 	args = parser.parse_args()
 
 	if not args.index_to_start_run:
-		'''
-		df = pd.read_csv("/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/example404/newicks_step1.csv",index_col=1)
-		for i, row in df.iterrows():
-			ind = row.name
-			g_id = ind.split(",")[0]
-			print(ind, ":", g_id)
-			df.loc[ind, "group_id"] = g_id
-		#exit()
-		df.to_csv("/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/example404/newicks_step1_with_ids.csv")
-		'''
 		df = pd.read_csv("/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/example404/newicks_step1_with_ids.csv",index_col=1)
 
 		group_ids_full = df["group_id"]
 		group_ids = group_ids_full.unique()
-		for group in group_ids[:4]:
+		for group in group_ids[4:]:
 			s = df.index[df["group_id"] == group].tolist()
 			submit_job_ll(s[0], len(s))
 	else:
