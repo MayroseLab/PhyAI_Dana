f6ad02b51f15387f24a2232b6c4944b77781537c
DanaAzouri <danaazouri@gmail.com>
2020-11-09 21:06:08 +0200

    -

--- utils/trying.py
+++ utils/trying.py
@@ -11,7 +11,7 @@ from subprocess import Popen, PIPE, STDOUT
 from parsing.parse_raxml_NG import parse_raxmlNG_content
 
 
-EXAMPLE_DIRNAME = 'example493/'
+EXAMPLE_DIRNAME = 'example403/'
 
 
 
@@ -141,7 +141,7 @@ if __name__ == '__main__':
 	exit()
 	'''
 	if not args.index_to_start_run:
-		#'''
+		'''
 		df = pd.read_csv("/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/{}newicks_step1.csv".format(EXAMPLE_DIRNAME),index_col=0)
 
 		for i, row in df.iterrows():
@@ -157,7 +157,7 @@ if __name__ == '__main__':
 		NROWS = len(df)
 		group_ids_full = df["group_id"]
 		group_ids = group_ids_full.unique()
-		for group in group_ids[:4]:
+		for group in group_ids[4:]:
 			s = df.index[df["group_id"] == group].tolist()
 			submit_job_ll(s[0], len(s), NROWS)
 
