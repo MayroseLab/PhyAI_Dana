a835c65d7440fb9f3736d830471adf6d794b1586
danaazouri <danaazouri@gmail.com>
2020-12-03 16:46:54 +0200

    in progress - automate the running forward algo

--- execute_programs/SPR_forward.py
+++ execute_programs/SPR_forward.py
@@ -6,7 +6,35 @@ from execute_programs.SPR_move import *
 
 
 
-def get_raxml_optimized_bl(tree_str, msa_file, rates, pinv, alpha, freq):
+def parse_raxmlNG_fortree(content):
+	"""
+	:return: dictionary with the attributes - string typed. if parameter was not estimated, empty string
+	"""
+	res_dict = dict.fromkeys(["ll", "tree"], "")
+
+	# likelihood
+	ll_re = re.search("Final LogLikelihood:\s+(.*)", content)
+	if ll_re:
+		res_dict["ll"] = ll_re.group(1).strip()
+	else:
+		res_dict["ll"] = 'unknown raxml-ng error, check "parse_raxmlNG_content" function'
+
+	# optimized tree string
+	tree_re = re.search("xx:\s+(.*)", content)
+	if tree_re:
+		res_dict["tree"] = tree_re.group(1).strip()
+	else:
+		res_dict["tree"] = 'unknown raxml-ng error, check "parse_raxmlNG_content" function'
+
+
+
+	return res_dict
+
+
+
+
+
+def get_raxml_optimized_bl(tree_str: object, msa_file: object, rates: object, pinv: object, alpha: object, freq: object) -> object:
 	model_line_params = 'GTR{rates}+I{pinv}+G{alpha}+F{freq}'.format(rates="{{{0}}}".format("/".join(rates)),
 									 pinv="{{{0}}}".format(pinv), alpha="{{{0}}}".format(alpha),
 									 freq="{{{0}}}".format("/".join(freq)))
@@ -23,9 +51,9 @@ def get_raxml_optimized_bl(tree_str, msa_file, rates, pinv, alpha, freq):
 		exit()
 
 		# todo: parse to return also tree str
-		res_dict = parse_raxmlNG_content(raxml_output)
+		res_dict = parse_raxmlNG_fortree(raxml_output)
 		ll = res_dict['ll']
-		rtime = res_dict['time']
+		tree_str = res_dict['tree']
 
 	except Exception as e:
 		print(msa_file.split(SEP)[-1][3:])
@@ -35,7 +63,7 @@ def get_raxml_optimized_bl(tree_str, msa_file, rates, pinv, alpha, freq):
 		os.remove(tree_rampath)
 
 	# todo: return also (optimized) tree str
-	return ll, rtime
+	return ll, tree_str
 
 
 
@@ -60,8 +88,8 @@ if __name__ == '__main__':
 	stats_filepath = dataset_path + PHYML_STATS_FILENAME.format("bionj")
 	params_dict = (parse_phyml_stats_output(None,stats_filepath))
 	freq, rates, pinv, alpha = [params_dict["fA"], params_dict["fC"], params_dict["fG"], params_dict["fT"]], [params_dict["subAC"], params_dict["subAG"], params_dict["subAT"], params_dict["subCG"], params_dict["subCT"],params_dict["subGT"]], params_dict["pInv"], params_dict["gamma"]
-	tree_str = get_raxml_optimized_bl(tree_str_not_opt, dataset_path+MSA_PHYLIP_FILENAME, rates, pinv, alpha, freq)
-	res = all_SPR(dataset_path, outpath, tree=tree_str, rewrite_phylip=args.rewrite_in_phylip)
+	ll, tree_str = get_raxml_optimized_bl(tree_str_not_opt, dataset_path+MSA_PHYLIP_FILENAME, rates, pinv, alpha, freq)
+	res = all_SPR(dataset_path, outpath, tree=tree_str)
 
 	collect_features(dataset_path, args.tree_relpath, outpath.format("prune"), outpath.format("rgft"))
 	# todo: run RF and extract the best 5 best predictions
