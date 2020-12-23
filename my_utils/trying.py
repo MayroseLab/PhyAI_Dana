c30fda1cca798ae34a7536eb76846c26f837f00e
DanaAzouri <danaazouri@gmail.com>
2020-12-23 17:10:35 +0200

    -

--- my_utils/trying.py
+++ my_utils/trying.py
@@ -153,12 +153,12 @@ if __name__ == '__main__':
 		df.to_csv("/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/{}newicks_step1_with_ids.csv".format(EXAMPLE_DIRNAME))
 		exit()
 		'''
-		df = pd.read_csv("/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/{}newicks_step1_with_ids.csv".format(EXAMPLE_DIRNAME)).dropna() #,index_col=0)
+		df = pd.read_csv("/groups/itay_mayrose/danaazouri/PhyAI/DBset2/data/training_datasets/{}newicks_step1.csv".format(EXAMPLE_DIRNAME), nrows=20000).dropna() #,index_col=0)
 
 		NROWS = len(df)
 		group_ids_full = df["group_id"]
 		group_ids = group_ids_full.unique()
-		for group in group_ids[:4]:
+		for group in group_ids[:-1]:
 			s = df.index[df["group_id"] == group].tolist()
 			submit_job_ll(s[0], len(s), NROWS)
 
